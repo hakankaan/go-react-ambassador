e94f696a8b15be2c7a24f5d211033f6f5d3ad509
hakankaan <kaanhakan2005@gmail.com>
2021-10-23 15:10:04 +0300

    feat: added Auth middleware

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -96,16 +96,9 @@ func Login(c *fiber.Ctx) error {
 func User(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+	token, _ := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
-
-	if err != nil || !token.Valid {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
-	}
 
 	payload := token.Claims.(*jwt.StandardClaims)
 
